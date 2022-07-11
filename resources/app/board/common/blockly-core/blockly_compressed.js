6e1d884b71a602f87b7c4721fb3ea20a3fa1735c
13940069056 <3294713004@qq.com>
2022-07-11 11:40:11 +0800

    Remove some invalid SourceMappingurl

--- resources/app/board/common/blockly-core/blockly_compressed.js
+++ resources/app/board/common/blockly-core/blockly_compressed.js
@@ -1793,7 +1793,5 @@ Blockly.Themes.Tritanopia.categoryStyles={colour_category:{colour:"#05427f"},lis
 return Blockly;
 }));
 
-
-//# sourceMappingURL=blockly_compressed.js.map
 Blockly.fireUiEventNow=function(a,b){var c=Blockly.fireUiEvent.DB_[b];if(c){var d=c.indexOf(a);-1!=d&&c.splice(d,1)}if(document.createEvent)c=document.createEvent("UIEvents"),c.initEvent(b,!0,!0),a.dispatchEvent(c);else if(document.createEventObject)c=document.createEventObject(),a.fireEvent("on"+b,c);else throw"FireEvent: No event creation mechanism.";};
 Blockly.fireUiEvent=function(a,b){var c=Blockly.fireUiEvent.DB_[b];if(c){if(-1!=c.indexOf(a))return;c.push(a)}else Blockly.fireUiEvent.DB_[b]=[a];setTimeout(function(){Blockly.fireUiEventNow(a,b)},0)};Blockly.fireUiEvent.DB_={};
