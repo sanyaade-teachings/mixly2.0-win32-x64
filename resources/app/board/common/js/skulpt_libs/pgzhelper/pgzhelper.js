dbd59b6c3e822430e4ef997fac47467314825cb9
王立帮 <3294713004@qq.com>
2022-08-10 10:56:38 +0800

    Remove resources

--- resources/app/board/common/js/skulpt_libs/pgzhelper/pgzhelper.js
+++
@@ -1,5 +0,0 @@
-var $builtinmodule = function (name) {
-	let pgzhelper= {__name__: new Sk.builtin.str("pgzhelper")};
-    
-	return pgzhelper;
-}
--- resources/app/static-server/server.js
+++
@@ -1,2 +0,0 @@
-const StaticServer = require("./static-server.js");
-StaticServer.run(9007, false);
